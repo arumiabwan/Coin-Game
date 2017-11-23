c6795a52e9ae035de9ce90d863f27e9bc83ae36d
aluminum88 <hogehoge@gmail.com>
2017-11-23 20:13:35 +0900

    Pusherとcoinの物理法則を操作して摩擦が発生するようにした

--- CoinGame/Assets/Pusher_move.cs
+++ CoinGame/Assets/Pusher_move.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class Pusher_move : MonoBehaviour {
 
     private Vector3 origin;
+    public Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-
         origin = transform.position;
+        rb = this.GetComponent<Rigidbody>();
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 offset = new Vector3(0, 0, 3*Mathf.Sin(Time.time*3/2)-1);
+        Vector3 offset = new Vector3(0, 0, 3*Mathf.Sin(Time.time)-1);
 
-        transform.position = origin + offset;
-	
-	}
+        //transform.position = origin + offset;
+        rb.MovePosition(origin + offset);
+      
+
+        
+
+
+    }
 }
