826b9fda5c91d82a68a912ca96228e3f3b6a3d9a
aluminum88 <hogehoge@gmail.com>
2017-11-23 18:43:08 +0900

    coinとZ/Xボタンでcoinを生成するCoinAddrを追加

---
+++ CoinGame/Assets/AddCoinScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCoinScript : MonoBehaviour {
+
+    public Transform Basecoin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Z) || Input.GetKeyDown(KeyCode.X)) {
+
+            Vector3 offset = new Vector3(0, 0, 1);
+
+            Instantiate(Basecoin, transform.position + offset, transform.rotation);
+            //生成するオブジェクト（コピー）,このオブジェクトの座標、回転を反映
+
+        }
+
+	}
+}
