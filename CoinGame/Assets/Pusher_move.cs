e19b158cf3267101a90f7993a3bc9028fb611d2e
aluminum88 <hogehoge@gmail.com>
2017-11-23 13:37:25 +0900

    Pusherの追加、無視するファイルの追加

---
+++ CoinGame/Assets/Pusher_move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher_move : MonoBehaviour {
+
+    private Vector3 origin;
+
+	// Use this for initialization
+	void Start () {
+
+        origin = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 offset = new Vector3(0, 0, 3*Mathf.Sin(Time.time*3/2)-1);
+
+        transform.position = origin + offset;
+	
+	}
+}
