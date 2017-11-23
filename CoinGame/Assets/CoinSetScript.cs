19d0b0529d5f6e002ff78f968d61f543265e6baf
aluminum88 <hogehoge@gmail.com>
2017-11-24 00:26:51 +0900

    起動時にある程度のコインを生成して場におくCoinSet追加（かなり適当）

---
+++ CoinGame/Assets/CoinSetScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSetScript : MonoBehaviour {
+
+    public Transform Basecoin;
+
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 0; i < 150; i++)
+        {
+            Vector3 offset = new Vector3(Random.value * 10, 0, Random.value * 5);
+            Instantiate(Basecoin, transform.position +offset , transform.rotation);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.C)) {
+
+            for (int i = 0; i < 100; i++)
+            {
+                Vector3 offset = new Vector3(Random.value * 10, 0, Random.value * 5);
+                Instantiate(Basecoin, transform.position + offset, transform.rotation);
+            }
+
+        }
+
+	}
+}
