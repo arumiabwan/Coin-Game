6f22c4fc63e3ce3b32a59145a64c15645378b3b6
aluminum88 <hogehoge@gmail.com>
2017-11-30 16:56:48 +0900

    pusherと動くセンサーとライトを追加（当たり判定はまだ）

---
+++ CoinGame/Assets/Sensor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour {
+
+    private Vector3 origin;
+
+    public static Vector3 offset_sensor;
+
+    // Use this for initialization
+    void Start () {
+
+        origin = transform.position;
+        offset_sensor = transform.position;
+         
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //offset = pm1.offsetcopy;
+        //offset = new Vector3(0, 0, 3 * Mathf.Sin(Time.time) - 1);
+       
+        transform.position = origin + offset_sensor;
+
+	}
+}
---
+++ CoinGame/Assets/Sensor_LightScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor_LightScript : MonoBehaviour {
+
+    private Vector3 origin;
+    public static Vector3 offset_sensor_light;
+
+	// Use this for initialization
+	void Start () {
+        origin = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = origin + offset_sensor_light;
+
+	}
+}
